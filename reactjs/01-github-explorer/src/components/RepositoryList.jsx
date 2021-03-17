043b27bdb1da09be93ccb9014158216074e685d9
itallosavieira <itallo.sa.vieira@gmail.com>
2021-03-17 11:42:57 -0300

    Create first props

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,29 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/itallosavieira'
+}
+
 export function RepositoryList() {
   return (
-    <section className="repository-list">
+    <section className='repository-list'>
       <h1> Repository List </h1>
 
       <ul>
-        <li>
-          <strong> unform </strong>
-          <p> Forms in React </p>
-
-          <a href="">Access Repository</a>
-        </li>
-
-        <li>
-          <strong> unform </strong>
-          <p> Forms in React </p>
-
-          <a href="">Access Repository</a>
-        </li>
-
-        <li>
-          <strong> unform </strong>
-          <p> Forms in React </p>
-
-          <a href="">Access Repository</a>
-        </li>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
